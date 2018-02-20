09545f37e2194d1a6f006f4b04e714e1c9d3e167
Patrick Tuffy <tuffyp@tcd.ie>
2018-02-20 17:36:00 +0000

    ini

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+'''comment added'''
+
+setup(name="systeminfo",
+      version ="0.1",
+      description="Basic system information for COMP30670",
+      url="",
+      author="Patrick Tuffy",
+      author_email="patrick.tuffy@ucdconnect.ie",
+      licence="GPL3",
+      packages=['systeminfo'],
+      entry_points={
+          'console_scripts':['comp30670_systeminfo=systeminfo.main:main']
+          }
+      )
---
+++ systeminfo/main.py
@@ -0,0 +1,13 @@
+'''
+Created on 30 Jan 2018
+lelelel
+@author: paddytuffy
+'''
+import platform
+
+def main():
+    print(platform.platform())
+    return
+if __name__ == '__main__':
+    main()
+    
